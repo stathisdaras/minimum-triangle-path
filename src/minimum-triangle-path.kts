de472ff0cd36f2814df5d527165914c908126df2
Stathis Daras <stathis.daras@upstreamsytstems.com>
2018-11-04 20:31:18 +0200

    ADD: Minimum path calculation - Added recursive method that calculates bottom to top the minimal path of the triangle - Script calculates the minimum path and prints result in the requested format

--- src/minimum-triangle-path.kts
+++ src/minimum-triangle-path.kts
@@ -5,6 +5,45 @@ calculateAndPrintMinPath()
 fun calculateAndPrintMinPath() {
     // Convert file into a list of lists of integers for further processing
     var triangleList = readAndCovertFile(validateInputAndReturnFileName())
+
+    var minPathList = ArrayList<Int>()
+    calculateMinPathRecursively(minPathList, triangleList)
+
+    println("Minimal path is: ${minPathList.displaySumAsString()} = ${minPathList.sum()}")
+
+}
+
+/**
+ * Extended Arraylist function to display sum of each element individually using '+' operator after each element
+ */
+fun <T> ArrayList<T>.displaySumAsString(): String {
+    var sb = StringBuilder()
+    this.forEach {
+        sb.append(it)
+        if (this.indexOf(it) != this.size - 1) sb.append(" + ")
+    }
+
+    return sb.toString()
+}
+
+/**
+ * Recursive methods that calculates minimu path of a triangle represented as list of lists, staring from the bottom.
+ *
+ * Reads last row and finds minimum, then for every pair of adjacent nodes of the min re-calculates recursively.
+ *
+ */
+fun calculateMinPathRecursively(minPathList: ArrayList<Int>, triagleList: List<List<Int>>, adjacentNodes: Pair<Int, Int>? = null) {
+    val currentRow = (if (adjacentNodes == null) triagleList.lastOrNull()
+    else triagleList.lastOrNull()?.subList(adjacentNodes.first, adjacentNodes.second + 1)) ?: return
+
+    val min = currentRow.min()
+    val minIndex = currentRow.indexOf(min)
+
+    if (min != null) {
+        minPathList.add(min)
+    }
+
+    calculateMinPathRecursively(minPathList, triagleList.subList(0, triagleList.size - 1), if (minIndex > 0) Pair(minIndex - 1, minIndex) else Pair(0, 0))
 }
 
 /**
@@ -30,7 +69,7 @@ fun validateInputAndReturnFileName(): String {
 
     // Check if fileName corresponds to a valid file
     val fileName = args[0]
-    if(!File(fileName).exists()) throw IllegalArgumentException("No valid file name passed.")
+    if (!File(fileName).exists()) throw IllegalArgumentException("No valid file name passed.")
 
     return fileName
 }
